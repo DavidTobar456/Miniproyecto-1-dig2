e06304591c7e4f4b8b737e9291bc3a54a70d86a3
DavidTobar456 <88524503+DavidTobar456@users.noreply.github.com>
2022-02-21 00:10:03 -0600

    Add files via upload

---
+++ PIC4/PIC4P1.X/build/default/production/_ext/2063596089/PIC4P1main.p1.d
@@ -0,0 +1,3 @@
+build/default/production/_ext/2063596089/PIC4P1main.p1:  \
+C:/Users/david/DavidU/ElectronicaDigital2/Proyecto1/PIC4/PIC4P1.X/PIC4P1main.c  \
+C:/Users/david/DavidU/ElectronicaDigital2/Proyecto1/PIC4/PIC4P1.X/../../../Laboratorio2/Laboratorio/Lab2dig2.X/confPIC16F887.h 
